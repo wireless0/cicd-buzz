880ab603cb6e70a4551918c746265227298c2e73
wireless0 <dsavtsa@gmail.com>
2020-01-02 20:05:24 +0100

    Fixed tests

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -19,7 +19,3 @@ def test_sample_multiple_words():
 def test_generate_buzz_of_at_least_five_words():
     phrase = generator.generate_buzz()
     assert len(phrase.split()) >= 5
-
-def test_generate_buzz_mongo_of_at_least_five_words():
-    phrase = generator.generate_buzz_mongo(MongoClient()['buzz']['generator'])
-    assert len(phrase.split()) >= 5
